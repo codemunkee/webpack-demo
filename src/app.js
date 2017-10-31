2284c05f9bd35fc9924ac8956f7d7506720d98a9
Russ Nealis <codemunkee@gmail.com>
2017-10-30 19:15:21 -0700

    now with people

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
-const root = document.querySelector('#root');
-root.innerHTML = `<p>Hello webpack.</p>`;
+// src/app.js
+import {groupBy} from 'lodash/collection';
+
+const people = [{
+  manager: 'Jen',
+  name: 'Bob'
+}, {
+  manager: 'Jen',
+  name: 'Sue',
+}, {
+  manager: 'Bob',
+  name: 'Shirley',
+}, {
+  manager: 'Bob',
+  name: 'Terrence'
+}];
+
+const managerGroups = groupBy(people, 'manager');
+
+const root = document.querySelector('#root')
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
